4d2f8762c81f738ac1b9e4eb633539058cf0cabd
joshunrau <contact@joshuaunrau.com>
2024-03-18 12:01:05 -0400

    test: add tests

--- lib/index.test-d.ts
+++ lib/index.test-d.ts
@@ -1,23 +1,114 @@
-import { expectType } from 'tsd';
+// import _ from 'lodash';
+import { expectAssignable, expectType } from 'tsd';
 
-import type { FormContent, FormFields } from './index.d.ts';
+import type { FormContent, FormFields, FormFieldsGroup, UnknownFormField } from './index.d.ts';
 
 type MockFormData = {
-  isWorkingCheck: boolean;
-  isWorkingRadio: boolean;
+  booleanCheckbox: boolean;
+  booleanRadio: boolean;
+  composite: {
+    compositeItem: any;
+  }[];
+  enum: 'a' | 'b' | 'c';
+  numberDefault: number;
+  numberSlider: number;
+  textLong: string;
+  textPassword: string;
+  textShort: string;
 };
 
-const mockFormContent: FormContent<MockFormData> = {
-  isWorkingCheck: {
+const mockFormFields: FormFields<MockFormData> = {
+  booleanCheckbox: {
     kind: 'boolean',
     label: 'Are the types working correctly?',
     variant: 'checkbox'
   },
-  isWorkingRadio: {
+  booleanRadio: {
     kind: 'boolean',
     label: 'Are the types working correctly?',
-    variant: 'checkbox'
+    variant: 'radio'
+  },
+  composite: {
+    fieldset: {
+      compositeItem: {
+        kind: 'text',
+        label: 'Are the types working correctly?',
+        variant: 'short'
+      }
+    },
+    kind: 'composite',
+    label: 'Are the types working correctly?'
+  },
+  enum: {
+    kind: 'enum',
+    label: 'Are the types working correctly?',
+    options: {
+      a: 'a',
+      b: 'b',
+      c: 'c'
+    }
+  },
+  numberDefault: {
+    kind: 'number',
+    label: 'Are the types working correctly?',
+    variant: 'default'
+  },
+  numberSlider: {
+    kind: 'number',
+    label: 'Are the types working correctly?',
+    max: 10,
+    min: 1,
+    variant: 'slider'
+  },
+  textLong: {
+    kind: 'text',
+    label: 'Are the types working correctly?',
+    variant: 'long'
+  },
+  textPassword: {
+    kind: 'text',
+    label: 'Are the types working correctly?',
+    variant: 'password'
+  },
+  textShort: {
+    kind: 'text',
+    label: 'Are the types working correctly?',
+    variant: 'short'
   }
 };
 
-expectType<FormFields<MockFormData>>(mockFormContent);
+const mockFormFieldsGroup: FormFieldsGroup<MockFormData> = {
+  fields: mockFormFields,
+  title: 'Mock Group'
+};
+
+// FormContent
+expectAssignable<FormContent<MockFormData>>(mockFormFields);
+expectAssignable<FormContent<MockFormData>>([mockFormFieldsGroup]);
+
+// FormFieldsGroup
+expectType<FormFieldsGroup<MockFormData>>(mockFormFieldsGroup);
+
+// FormFields
+expectType<FormFields<MockFormData>>(mockFormFields);
+
+// UnknownFormField
+expectAssignable<UnknownFormField>(mockFormFields.booleanCheckbox);
+expectAssignable<UnknownFormField>(mockFormFields.booleanRadio);
+expectAssignable<UnknownFormField>(mockFormFields.enum);
+expectAssignable<UnknownFormField>(mockFormFields.numberDefault);
+expectAssignable<UnknownFormField>(mockFormFields.numberSlider);
+expectAssignable<UnknownFormField>(mockFormFields.textLong);
+expectAssignable<UnknownFormField>(mockFormFields.textPassword);
+expectAssignable<UnknownFormField>(mockFormFields.textShort);
+
+// PrimitiveFormField
+expectType<'boolean' | 'dynamic'>(mockFormFields.booleanCheckbox.kind);
+expectType<'boolean' | 'dynamic'>(mockFormFields.booleanRadio.kind);
+expectType<'composite' | 'dynamic'>(mockFormFields.composite.kind);
+expectType<'dynamic' | 'enum' | 'text'>(mockFormFields.enum.kind);
+expectType<'dynamic' | 'number'>(mockFormFields.numberDefault.kind);
+expectType<'dynamic' | 'number'>(mockFormFields.numberSlider.kind);
+expectType<'dynamic' | 'enum' | 'text'>(mockFormFields.textLong.kind);
+expectType<'dynamic' | 'enum' | 'text'>(mockFormFields.textPassword.kind);
+expectType<'dynamic' | 'enum' | 'text'>(mockFormFields.textShort.kind);
