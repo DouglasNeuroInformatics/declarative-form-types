b100f83e954fafc89fe76feeaa8bc73627a40903
joshunrau <contact@joshuaunrau.com>
2024-03-19 15:26:13 -0400

    ci: add changelog and npm to workflow

--- release.config.js
+++ release.config.js
@@ -1,19 +1,35 @@
 // @ts-check
 
+const preset = 'conventionalcommits';
+
 /** @type {import('semantic-release').GlobalConfig} */
 export default {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', { preset }],
+    ['@semantic-release/release-notes-generator', { preset }],
+    ['@semantic-release/changelog', { changelogTitle: 'Changelog' }],
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        tarballDir: 'dist'
+      }
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'dist/*.tgz'
+      }
+    ],
     [
       '@semantic-release/git',
       {
-        assets: ['lib', 'package.json'],
+        assets: ['package.json', 'CHANGELOG.md'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
-    ],
-    '@semantic-release/github'
+    ]
   ],
   repositoryUrl: 'https://github.com/DouglasNeuroInformatics/libui-form-types.git',
   tagFormat: 'v${version}'
